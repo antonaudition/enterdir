922e91e333a34c6e76c49421c4056e6c6e151357
Anton Hendriks <anton.hendriks@gmail.com>
2017-06-03 14:35:46 +0200

    stat working

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 build:
-	mvn clean install
+	mvn install
 
-docker-build:
+run: build
+	java -jar ./target/entersekt-dir-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+docker-build: build
 	docker build -t enterdir .
 
-run:
+docker-run: docker-build
 	docker run -p 8080:4567 enterdir
 
--- src/main/java/com/entersekt/dir/Main.java
+++ src/main/java/com/entersekt/dir/Main.java
@@ -1,10 +1,28 @@
 package com.entersekt.dir;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        port(8080);
+        get("/alive", (req, res) -> "I'm alive!\n");
+        get("/stat/*", (req, res) -> {
+            Path path = arrayToPath(req.splat());
+            Boolean exists = Files.isReadable(path);
+            return path.toString() + " exists? " + exists.toString() +"\n";
+        });
+    }
+
+    private static Path arrayToPath(String[] parts) {
+        return Paths.get(Arrays.toString(parts)
+                .replace(", ", "/")
+                .replace("[", "/")
+                .replace("]", ""));
     }
 }
